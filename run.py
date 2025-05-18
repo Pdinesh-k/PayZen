5f72b134ce86e199058257cd1caf544e42b5053e
P Dineshkumar <cheenu2801@gmail.com>
2025-05-18 11:12:56 +0530

    Run removed

--- run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "app.main:app",  # Use import string format
-        host="0.0.0.0",
-        port=8000,
-        reload=True  # Enable auto-reload for development
-    ) 
